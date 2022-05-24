fa9d58a8beead1b96ece545ec979d5e826afd439
yurcn <105837812+yurcn@users.noreply.github.com>
2022-05-25 00:10:44 +0700

    * moved replacements to config

--- mysql2loki.py
+++ mysql2loki.py
@@ -8,41 +8,60 @@ import re
 from time import sleep
 from multiprocessing import Queue
 
-with open("mysql2loki-config.yml", "r") as ymlconfig:
-    cnf = yaml.safe_load(ymlconfig)
+def time2tag(time):
+    if int(time) < 3:
+        return '1_3'
+    elif 3 < int(time) <= 5:
+        return '3_5'
+    elif 5 < int(time) <= 10:
+        return'5_10'
+    elif 10 < int(time) <= 20:
+        return '10_20'
+    elif 20 < int(time) <= 30:
+        return '20_30'
+    else:
+        return '30_'        
 
-handler = logging_loki.LokiQueueHandler(
-    Queue(-1),
-    url= cnf['loki']['url'],
-    tags={"instance": cnf['name'],},
-    version="1",
-)
+def process_replacements(stmt, replacements):
+    if len(replacements) > 0:
+        trimmed = len(stmt)
+        for pattern in replacements:
+            for q in replacements[pattern]:
+                stmt = re.sub(q['search'], q['replace'], stmt)
+    if len(stmt) > 1000:
+        last_closing_par = stmt.rfind(')')
+        last_opening_par = stmt.rfind('(')
+        if last_opening_par > last_closing_par:
+            stmt = stmt[:980] + '-- long line trimmed'
+        elif last_closing_par > 700:
+            stmt = stmt[:680] + '/* long line trimmed */' + stmt[last_closing_par-1:]
+    trimmed -= len(stmt)
+    if trimmed > 0 and 'long line trimmed' in stmt:
+        stmt = re.sub(r'long line trimmed', str(trimmed) + ' chars trimmed', stmt)
+    return stmt
 
-try:
-    period = cnf['period']
-except KeyError:
-    period = 150
+def read_config(config_file):
+    with open(config_file, "r") as ymlconfig:
+        cnf = yaml.safe_load(ymlconfig)
+    handler = logging_loki.LokiQueueHandler(
+        Queue(-1),
+        url= cnf['loki']['url'],
+        tags={"instance": cnf['name'],},
+        version="1",
+    )
+    period = cnf.get('period', 150)
+    logger_name = cnf.get('logger', 'SlowQueriesSnap')
+    port = cnf['mysql'].get('port', 3306)
+    log_key = cnf['mysql'].get('log_column', 'info')
+    replacements = cnf.get('replacements', {})
+    extra_tags = {"severity": "info",}
+    return handler, cnf, period, logger_name, port, log_key, replacements, extra_tags
 
-try:
-    logger_name = cnf['logger'] 
-except KeyError:
-    logger_name = 'SlowQueriesSnap'
-
-try:
-    port = cnf['mysql']['port']
-except KeyError:
-    port = 3306
-
-try:
-    log_key = cnf['mysql']['log_column']
-except KeyError:
-    log_key = 'info'
-
-extra_tags = {"severity": "info",}
+handler, cnf, period, logger_name, port, log_key, replacements, extra_tags = read_config("mysql2loki-config.yml")
 
 while True:
-    connection = pymysql.connect( host=cnf['mysql']['host'], user=cnf['mysql']['user'], password=cnf['mysql']['pass'],database=None, port=port, cursorclass=pymysql.cursors.DictCursor)
     try:
+        connection = pymysql.connect(host=cnf['mysql']['host'], user=cnf['mysql']['user'], password=cnf['mysql']['pass'], database=None, port=port, cursorclass=pymysql.cursors.DictCursor)
         with connection.cursor() as cur:
             cur.execute(cnf['mysql']['query'])
             numrows = cur.rowcount
@@ -50,25 +69,16 @@ while True:
                 row = cur.fetchone()
                 if row is None or log_key not in row:
                     continue
-                if len(row[log_key]) > 0:
-                    if logger_name == 'SlowQueries':
-                        stmt = re.sub(r'IN\s+\([\(\),0123456789]{20,}$', r'IN (-- long line trimmed', str(row[log_key]))
-                        stmt = re.sub(r'VALUES\s*\([\(\),0123456789]{20,}$', r'VALUES(-- long line trimmed', stmt)
-                        if int(row['time']) > 30:
-                            timetag = '30_'
-                        elif 20 < int(row['time']) <= 30:
-                            timetag = '20_30'
-                        elif 10 < int(row['time']) <= 20:
-                            timetag = '10_20'
-                        elif 5 < int(row['time']) <= 10:
-                            timetag = '5_10'                    
-                        elif 3 < int(row['time']) <= 5:
-                            timetag = '3_5'
-                        else:
-                            timetag = '1_3'
-                        stmt = str(format(row['time'],'02d')) + 's ' + stmt 
+                stmt = str(row[log_key])
+                if len(stmt) > 0:
+                    if len(replacements) > 0:
+                        stmt = process_replacements(stmt,replacements)
+                    if 'time' in row:
+                        timetag = time2tag(row['time'])
+                        stmt = str(format(row['time'], '02d')) + 's ' + stmt
                         extra_tags['timetag'] = timetag
-                        extra_tags['host'] = str(row['host']).split(":")[0]
+                    if 'host' in row:
+                        extra_tags['host'] = str(row['host']).split(":")[0]                        
                     if 'extra_tags' in cnf['mysql']:
                         for tag in cnf['mysql']['extra_tags']:
                             if tag in row:
